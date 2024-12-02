fe1f49ac4aab7d9f74157443b1a86581ef883b9c
YOGEESH K P <yogeesh793@gmail.com>
2024-12-02 12:08:53 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,30 @@
+# Strings
+# concatination
+first_name="josh"
+last_name="in"
+full_name=first_name+" "+last_name
+print(full_name)
+
+# repetation
+greeting = "hello! "*3
+print(greeting)
+
+# string method
+message = "hello, world"
+print(message.strip())
+print(message.upper())
+print(message.lower())
+print(message.replace("world", "python"))
+
+a="yogee say 'hello'"
+print(a)
+
+b="""yogee say 'hello'
+krishna said 'hi'"""
+print(b)
+
+c="yogee \n say hi"
+print(c)
+
+c="yogee \t say hi"
+print(c)
